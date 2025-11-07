45c1641c4c0490dc50854aacc9a4bfe751df6413
jwhite <jtwhite1000@gmail.com>
2025-11-07 14:10:11 -0600

    retooling ext run mgr

--- src/libs/run_managers/external/RunManagerExternal.h
+++ src/libs/run_managers/external/RunManagerExternal.h
@@ -31,12 +31,12 @@ public:
 	RunManagerExternal(const std::vector<std::string> _comline_vec,
 		const std::vector<std::string> _tplfile_vec, const std::vector<std::string> _inpfile_vec,
 		const std::vector<std::string> _insfile_vec, const std::vector<std::string> _outfile_vec,
-		const std::string &stor_filename, const std::string &_ext_filename, const std::string &_exi_filename, int _max_n_failure = 1);
+		const std::string &stor_filename, const std::string &_ext_filename, const std::string &_exi_filename,
+		int _max_n_failure = 1,int _sleep_ms=10);
 	virtual ~RunManagerExternal();
 	virtual void run();
 private:
-	std::string ext_filename;
-	std::string exi_filename;
+	int sleep_ms;
 };
 
 #endif /* _RUNMANAGEEXTERNAL_H_ */
